e2e62ff7fc3310109eda9b28ce0c3444126d2739
Andrew Mititi <andymwebi@gmail.com>
2024-08-29 16:17:55 +0300

    Add function ServiceName

--- main.go
+++ main.go
@@ -1,16 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 var service_name string  = "main.go"
 const APP_NAME string = "golang module 1"
 
 func ServiceName(name string) string {
-	if name != "" {return name}
+	if name != "" {return strings.ToUpper(name)}
 	return service_name
 }
 
 func main(){ 
 	fmt.Printf("Running project %s\n", APP_NAME )
-	fmt.Printf("Hello from %s", ServiceName(""))
+	fmt.Printf("Hello from %s\n", ServiceName("Module 1"))
 }
