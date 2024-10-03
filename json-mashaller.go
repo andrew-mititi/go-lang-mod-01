34b8129a7b0e73ad4a14a64d0a5b6a250b5a9a23
Andrew Mititi <andymwebi@gmail.com>
2024-10-03 12:44:56 +0300

    Json marshaller

--- json-mashaller.go
+++ json-mashaller.go
@@ -1,8 +1,66 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+	"sort"
+)
 
+type user struct {
+	First, Last string
+	Age         int
+}
 
-func run(){
-	fmt.Println(`Json Marshaller`)
+type userjson struct {
+	First string `json:"First"`
+	Last  string `json:"Last"`
+	Age   int    `json:"Age"`
+}
+
+type byAge []user
+
+func (u byAge) Len() int {
+	return len(u)
+}
+
+func (u byAge) Less(i, j int) bool {
+	return u[i].Age < u[j].Age
+}
+
+func (u byAge) Swap(i, j int) {
+	u[i], u[j] = u[j], u[i]
+}
+
+func run() {
+	u1 := user{
+		First: "Andrew",
+		Last:  "Mititi",
+		Age:   26,
+	}
+
+	u2 := user{
+		First: "Rael",
+		Last:  "Gesare",
+		Age:   1,
+	}
+
+	users_array := []user{u1, u2}
+
+	_, err := json.Marshal(users_array)
+	if err != nil {
+		log.Fatal(err)
+	}
+	json.NewEncoder(os.Stdout).Encode(u1)
+
+	users_json_data := `[{"First":"Andrew","Last":"Mititi","Age":26},{"First":"Rael","Last":"Gesare","Age":1}]`
+	d := []userjson{}
+
+	err = json.Unmarshal([]byte(users_json_data), &d)
+	if err != nil {
+		log.Fatal(err)
+	}
+	sort.Sort(byAge(users_array))
+	fmt.Println(users_array)
 }
